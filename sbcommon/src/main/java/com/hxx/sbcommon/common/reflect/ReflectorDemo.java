1613af6e14abc6793760f4332c1692af4e9623c9
huoxuxu <huoxuxu@zto.com>
2023-01-03 10:16:35 +0800

    add ReflectorDemo

--- sbcommon/src/main/java/com/hxx/sbcommon/common/reflect/ReflectorDemo.java
+++ sbcommon/src/main/java/com/hxx/sbcommon/common/reflect/ReflectorDemo.java
@@ -1,6 +1,10 @@
 package com.hxx.sbcommon.common.reflect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ReflectPermission;
 import java.util.*;
@@ -10,6 +14,7 @@ import java.util.*;
  * @Description:
  * @Date: 2022-08-30 8:59:55
  **/
+@Slf4j
 public class ReflectorDemo {
     private final Class type;
 //    private final String[] readablePropertyNames;
@@ -24,6 +29,67 @@ public class ReflectorDemo {
 
     }
 
+    // k=类名，v=所有Setter
+    private Map<String,List<FieldSetter>>setterMap = new HashMap<>();
+
+    public void initialize() {
+        // 从缓存获取接口类的setter列表
+        List<FieldSetter> setters = setterMap.get(this.getClass().getName());
+        // 如果还没有缓存、初始化接口类的setter列表
+        if (setters == null) {
+            setters = this.initSetters();
+        }
+        // 遍历接口类的setter
+        for (FieldSetter setter: setters) {
+            // 用setter的名字（也就是字段的名字）检索键值对
+            String fieldName = setter.getName();
+            String fieldValue = nodes.get(fieldName);
+            // 没有检索到键值对、或者键值对没有赋值，跳过
+            if (StringUtils.isEmpty(fieldValue)) continue;
+            try {
+                Method method = setter.getMethod();
+                // 用类反射方式调用setter
+                method.invoke(this, fieldValue);
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                log.error("It's failed to initialize field: {}, reason: {}", fieldName, e);
+                // 不能调用setter，可能是虚拟机回收了该子类的全部实例、入口地址变化，更新地址、再试一次
+                try {
+                    Method method = this.getSetter(fieldName);
+                    setter.setMethod(method);
+                    method.invoke(this, fieldValue);
+                } catch (Exception e1) {
+                    log.debug("It's failed to initialize field: {}, reason: {}", fieldName, e1);
+                }
+            } catch (Exception e) {
+                log.error("It's failed to initialize field: {}, reason: {}", fieldName, e);
+            }
+        }
+    }
+
+    protected List<FieldSetter> initSetters() {
+        String className = this.getClass().getName();
+        List<FieldSetter> setters = new ArrayList<>();
+        // 遍历类的可调用函数
+        for (Method method: this.getClass().getMethods()) {
+            String methodName = method.getName();
+            // 如果从名字推断是setter函数，添加到setter函数列表
+            if (methodName.startsWith("set")) {
+                // 反推field的名字
+                String fieldName = StringUtils.lowerFirstChar(methodName.substring(3));
+                setters.add(new FieldSetter(fieldName, method));
+            }
+        }
+        // 缓存类的setter函数列表
+        setterMap.put(className, setters);
+        // 返回可调用的setter函数列表
+        return setters;
+    }
+
+    private Method getSetter(String fieldName) throws NoSuchMethodException, SecurityException {
+        String methodName = String.format("set%s", StringUtils.upperFirstChar(fieldName));
+        // 获取field的setter，只要是用public修饰的setter、不管是自己的还是从父类继承的，都能取到
+        return this.getClass().getMethod(methodName, String.class);
+    }
 
     private void addDefaultConstructor(Class<?> clazz) {
         Constructor<?>[] consts = clazz.getDeclaredConstructors();
@@ -142,6 +208,17 @@ public class ReflectorDemo {
         }
 
         return sb.toString();
+    }
+
+    @lombok.Data
+    static class FieldSetter {
+        private String name;
+        private Method method;
+
+        public FieldSetter(String name, Method method) {
+            this.name = name;
+            this.method = method;
+        }
     }
 
 }
