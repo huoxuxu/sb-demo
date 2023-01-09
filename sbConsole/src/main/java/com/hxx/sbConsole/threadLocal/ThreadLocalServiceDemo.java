8c82d7d71418258a2ea7234cffea4e1e6f0e37b5
huoxuxu <huoxuxu@zto.com>
2023-01-09 14:34:13 +0800

    add NotThreadSafe

---
+++ sbConsole/src/main/java/com/hxx/sbConsole/threadLocal/ThreadLocalServiceDemo.java
@@ -0,0 +1,21 @@
+package com.hxx.sbConsole.threadLocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: huoxuxu
+ * @Description:
+ * @Date: 2023-01-06 9:30:24
+ **/
+@Slf4j
+@Service
+public class ThreadLocalServiceDemo {
+    public static void main(String[] args) {
+        String str="1,2,3,";
+        String[] split = str.split(",");
+        System.out.println(split.length);
+        System.out.println(String.join(",",split));
+
+    }
+}
