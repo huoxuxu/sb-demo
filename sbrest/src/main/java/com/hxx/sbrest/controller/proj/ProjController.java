eb5e2430b7e2388dacfde37ba96a2c61cb41d55f
huawu <h_xuxu@sina.com>
2023-07-24 08:40:31 +0800

    add ProjController

---
+++ sbrest/src/main/java/com/hxx/sbrest/controller/proj/ProjController.java
@@ -0,0 +1,44 @@
+package com.hxx.sbrest.controller.proj;
+
+import com.hxx.sbcommon.common.json.JsonUtil;
+import com.hxx.sbrest.model.T1Model;
+import com.hxx.sbrest.service.BasicTestService;
+import com.hxx.sbrest.webUtil.WebProjPowerful;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: huoxuxu
+ * @Description:
+ * @Date: 2021-05-07 17:40:20
+ **/
+@Slf4j
+@RestController
+@RequestMapping("proj")
+public class ProjController {
+
+    @Autowired
+    private WebProjPowerful webProjPowerful;
+
+    /**
+     * 查询项目暴漏的所Url
+     *
+     * @return
+     */
+    @RequestMapping("/getAllUrls")
+    public Collection<String> getT1() {
+        Set<String> allUrls = webProjPowerful.getAllUrl();
+        return allUrls.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+}
