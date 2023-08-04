33e3947e1babc07df10c33e166c15f09ed4f5d07
huawu <h_xuxu@sina.com>
2023-08-04 09:47:27 +0800

    rename DemoTestController

--- sbweb/src/main/java/com/hxx/sbweb/controller/DemoController.java
+++ sbweb/src/main/java/com/hxx/sbweb/controller/DemoController.java
@@ -1,25 +1,20 @@
 package com.hxx.sbweb.controller;
 
 import com.hxx.sbcommon.model.Result;
-import com.hxx.sbservice.model.JdbcModel;
-import com.hxx.sbservice.model.attr.Value.ValueTestConf;
-import com.hxx.sbweb.common.JsonUtil;
-import com.hxx.sbweb.service.ConfigurationPropertiesTestService;
-import com.hxx.sbweb.service.PropertySourceTestConfService;
+import com.hxx.sbweb.controller.base.BaseRestController;
+import com.hxx.sbweb.model.StuModel;
 import com.hxx.sbweb.service.impl.DemoMyBatisServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  * mybatis 注解方式示例
  */
 @RestController
 @RequestMapping("/demo")
-public class DemoTestController {
+public class DemoController extends BaseRestController {
     @Autowired
     private DemoMyBatisServiceImpl demoMyBatisService;
 
@@ -36,5 +31,11 @@ public class DemoTestController {
         return Result.success(ret);
     }
 
+    @RequestMapping("/case2")
+    public Result<String> case2(@RequestBody StuModel model) {
+        String ret = demoMyBatisService.demo();
+
+        return Result.success(ret);
+    }
 
 }
