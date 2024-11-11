de7383dd3bd71c747dfce9414fb9288b5d2a891a
huawu <h_xuxu@sina.com>
2024-11-11 13:59:03 +0800

    add CompletableFutureDemo.case0

--- sbConsole/src/main/java/com/hxx/sbConsole/thread/completablefuture/CompletableFutureDemo.java
+++ sbConsole/src/main/java/com/hxx/sbConsole/thread/completablefuture/CompletableFutureDemo.java
@@ -3,8 +3,41 @@ package com.hxx.sbConsole.thread.completablefuture;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
+/*
+CompletableFuture
+
+创建：
+# 无返回值
+CompletableFuture.runAsync
+# 有返回值
+CompletableFuture.supplyAsync
+
+任务执行完成后操作
+# 将前一个CompletableFuture的结果作为输入，然后返回一个新的值。新的值会被包装在一个新的CompletableFuture中返回
+thenApply
+# 将多个异步操作串联起来，后一个异步操作是基于前一个异步操作的结果动态创建的
+thenCompose
+
+执行
+# 等待所有的CompletableFuture都完成
+CompletableFuture.allOf
+# 等待任一CompletableFuture完成
+CompletableFuture.anyOf
+
+获取结果
+# 获取异步任务的结果，会抛出：InterruptedException和ExecutionException
+get()
+# 获取异步计算的结果，不会抛出InterruptedException和ExecutionException异常，但会抛出其他异常
+join()
+
+
+*/
 public class CompletableFutureDemo {
 
     public static void main(String[] args) {
@@ -14,6 +47,30 @@ public class CompletableFutureDemo {
         } catch (Exception ex) {
             System.out.println(ExceptionUtils.getStackTrace(ex));
         }
+    }
+
+    // 用法示例
+    private static List<String> case0() {
+        List<String> ls = new ArrayList<>();
+        ls.add("1");
+        ls.add("2");
+
+        List<CompletableFuture<String>> futures = ls.stream()
+                .map(param -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return "1";
+                    } catch (Exception e) {
+                        // 处理异常，返回默认值或进行其他错误处理操作
+                        return null;
+                    }
+                }).exceptionally(ex -> null))
+                .collect(Collectors.toList());
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList()))
+                .join();
     }
 
     // 创建CompletableFuture 示例
@@ -99,7 +156,7 @@ public class CompletableFutureDemo {
     }
 
     // 获取结果
-    private static void futureResult(){
+    private static void futureResult() {
         /*
             获取异步任务的结果
             get方法
@@ -109,6 +166,18 @@ public class CompletableFutureDemo {
              获取异步计算的结果，不会抛出InterruptedException和ExecutionException异常，但会抛出其他异常
         */
 
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                // 模拟一个耗时操作，比如网络请求或者数据库查询
+                Thread.sleep(2000);
+                return "Hello, World";
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        // 使用join方法获取结果，会阻塞当前线程直到future完成
+        String result = future.join();
+        System.out.println(result);
     }
 
     // 处理异常情况
